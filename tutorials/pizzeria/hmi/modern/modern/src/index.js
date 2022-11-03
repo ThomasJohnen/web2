824fb203639662a7593cbe6b6c54223b727de9e5
ThomasJohnen <thomas.johnen@hotmail.be>
2022-11-03 22:44:02 +0100

    Insertion du tableau avec InnerHTML

--- tutorials/pizzeria/hmi/modern/modern/src/index.js
+++ tutorials/pizzeria/hmi/modern/modern/src/index.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
 import 'animate.css';
 
-/* 
 
 const body = document.querySelector('body');
 
@@ -17,4 +16,79 @@ function startOrStopSound() {
   else myAudioPlayer.pause();
 }
 
-*/
+const MENU = [
+  {
+    id: 1,
+    title: '4 fromages',
+    content: 'Gruyère, Sérac, Appenzel, Gorgonzola, Tomates',
+  },
+  {
+    id: 2,
+    title: 'Vegan',
+    content: 'Tomates, Courgettes, Oignons, Aubergines, Poivrons',
+  },
+  {
+    id: 3,
+    title: 'Vegetarian',
+    content: 'Mozarella, Tomates, Oignons, Poivrons, Champignons, Olives',
+  },
+  {
+    id: 4,
+    title: 'Alpage',
+    content: 'Gruyère, Mozarella, Lardons, Tomates',
+  },
+  {
+    id: 5,
+    title: 'Diable',
+    content: 'Tomates, Mozarella, Chorizo piquant, Jalapenos',
+  },
+];
+
+
+
+function getAllTableLinesAsString(menu) {
+  let pizzaTableLines = '';
+
+
+  /* pourquoi dans l'incrémentation de pizzaTableLines on fait
+  `` et pas '' ? ...
+  */
+  menu?.forEach((pizza) => {
+    pizzaTableLines += `<tr>
+    <td>${pizza.title}</td>
+    <td>${pizza.content}</td>
+    </tr>`;
+  });
+
+  return pizzaTableLines;
+}
+
+function addLinesToTable(tableLines) {
+  const menuTable = `
+  <div class="table-responsive pt-5">
+    <table class="table table-danger">
+      <tr>
+        <th>Pizza</th>
+        <th>Description</th>
+      </tr>
+      ${tableLines}    
+    </table>
+  </div>
+  `;
+  return menuTable;
+}
+
+function getMenuTableAsString(menu) {
+  const menuTableLines = getAllTableLinesAsString(menu);
+  const menuTable = addLinesToTable(menuTableLines);
+  return menuTable;
+}
+
+function renderMenuFromString(menu) {
+  const menuTableAsString = getMenuTableAsString(menu);
+  const main = document.querySelector('main');
+
+  main.innerHTML += menuTableAsString;
+}
+
+renderMenuFromString(MENU);
