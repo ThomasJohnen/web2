255f03ddd640dac91116072eaf30a4d99c72c628
ThomasJohnen <thomas.johnen@hotmail.be>
2022-11-13 11:04:40 +0100

    debut pizzeria structured

---
+++ tutorials/pizzeria/hmi/Structured/src/index.js
@@ -0,0 +1,12 @@
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheets/main.css';
+import 'animate.css';
+
+import HomePage from './Components/Pages/HomePage';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+
+HomePage();
+Header();
+Footer();
